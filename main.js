5d57394ff254c7f659673227430f498a6ec3830e
belfor-dev <bel4negro@gmail.com>
2023-04-28 18:23:15 -0400

    update: logs guardados en variable

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const attacks = require("./attacks.json");
 const magicAttacks = attacks.filter(item => item.type === 'MAGIC');
 const physicalAttacks = attacks.filter(item => item.type === 'PHYSICAL');
 const classes = [ "MAGICIAN","FAIRY","KNIGHT","WARRIOR"]
-let logs = [];
+let logs_pelea = [];
 
 
 
@@ -55,7 +55,7 @@ function pickAtack(claseParametro){
 }
 
 function showFighters(){
-	push.logs(fighter1.name +" | Class: " +fighter1.clase + " | Health: " + fighter1.health + " VS " + fighter2.name +" | Class: " +fighter2.clase + " | Health: " + fighter2.health)
+	logs_pelea.push(fighter1.name +" | Class: " +fighter1.clase + " | Health: " + fighter1.health + " VS " + fighter2.name +" | Class: " +fighter2.clase + " | Health: " + fighter2.health)
 }
 
 function pickSpell(character){
@@ -74,12 +74,12 @@ function attack(atacker, defender){
     let probability = (spell.accuracy*multiplicator)
     
     if(probability>=450 && healthCheck()==true){
-        push.logs(" Turno: "+turno);
+        logs_pelea.push(" Turno: "+turno);
         defender.health -= spell.damage;
-        push.log(atacker.name + " ataca con " + spell.name + "... DA EN ELBLANCO! La vida de " + defender.name+" queda en " + defender.health)
+        logs_pelea.push(atacker.name + " ataca con " + spell.name + "... DA EN ELBLANCO! La vida de " + defender.name+" queda en " + defender.health);
     }else if(probability<500){
-        push.logs(" Turno: "+turno)
-        console.log(atacker.name + " ataca con " + spell.name + "... FALLÓ! La vida de " + defender.name+" se mantiene en " + defender.health)
+        logs_pelea.push(" Turno: "+turno);
+        logs_pelea.push(atacker.name + " ataca con " + spell.name + "... FALLÓ! La vida de " + defender.name+" se mantiene en " + defender.health);
         atacker.fails++;
     }
 }
@@ -87,15 +87,15 @@ function attack(atacker, defender){
 function healthCheck (){
     if (fighter1.health <= 0){
         fighter1.isDead = true;
-        console.log("EL COMBATE TERMINA!")
-        console.log(fighter1.name + " no puede continuar")
-        console.log(fighter2.name + " GANA LA BATALLA!")
+        logs_pelea.push("EL COMBATE TERMINA!")
+        logs_pelea.push(fighter1.name + " no puede continuar")
+        logs_pelea.push(fighter2.name + " GANA LA BATALLA!")
         return false;
     }if (fighter2.health<=0){
         fighter2.isDead = true;
-        console.log("EL COMBATE TERMINA!")
-        console.log(fighter2.name + " no puede continuar")
-        console.log(fighter1.name + " GANA LA BATALLA!")
+        logs_pelea.push("EL COMBATE TERMINA!")
+        logs_pelea.push(fighter2.name + " no puede continuar")
+        logs_pelea.push(fighter1.name + " GANA LA BATALLA!")
         return false;
     }else{
         return true
@@ -130,13 +130,14 @@ function fight(){
         }
     }
 
-    console.log(fighter1.name+ " falló " + fighter1.fails +" veces sus ataques");
-    console.log(fighter2.name+ " falló " + fighter2.fails +" veces sus ataques")
+    logs_pelea.push(fighter1.name+ " falló " + fighter1.fails +" veces sus ataques");
+    logs_pelea.push(fighter2.name+ " falló " + fighter2.fails +" veces sus ataques")
 
 
 }
 
 fight()
+console.log(logs_pelea)
 
 
 
